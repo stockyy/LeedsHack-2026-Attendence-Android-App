86a4dcb748834e716869010d63e1a4a9cb1c33d2
stock <fnnb5132@leeds.ac.uk>
2026-02-08 02:01:40 +0000

    Sign in working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
 
     // Compose UI
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -72,4 +72,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    implementation(libs.androidx.material.icons.extended)
 }
